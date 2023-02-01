4eed87d0fb9a0f2d94761e6f871becf8ba8c872a
Seth Brown <sethbrown@MacBook-Pro.local>
2023-02-01 16:02:49 -0700

    1 | Basic TSQ config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import type { AppProps } from 'next/app';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  // Create a TanstackQuery client
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+
+      {/* Dev tools for Tanstack Query */}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
